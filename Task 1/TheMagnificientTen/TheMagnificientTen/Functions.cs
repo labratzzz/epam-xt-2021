eadcf51dd527267646adc8cd2428d27ffd54f4e2
Michael Rodin <mikhail.rodin@satellite-soft.ru>
2021-03-03 15:12:09 +0400

    Added the rest of exercises

---
+++ Task 1/TheMagnificientTen/TheMagnificientTen/Functions.cs
@@ -0,0 +1,225 @@
+﻿using System;
+
+namespace TheMagnificientTen
+{
+    static class Functions
+    {
+        public static int RectangleSqaure(int a, int b)
+        {
+            if (a <= 0 || b <= 0) throw new ArgumentException("Given arguments must be greater than zero");
+            return a * b;
+        }
+
+        public static int SumOfNumbers(int limit, params int[] numbers)
+        {
+            if (limit <= 0) throw new ArgumentException("This param must be greater than zero", "limit");
+            if (numbers.Length == 0) throw new ArgumentException("At least one number must be specified", "numbers");
+            foreach (int number in numbers) if (number <= 0) throw new ArgumentException("This param must be greater than zero", "numbers");
+            
+            int result = 0;
+            for (int i = 0; i < limit; i++)
+            {
+                checked 
+                {
+                    foreach (int number in numbers)
+                    { 
+                        if (i % number == 0) result += i;
+                    }
+                }
+            }
+            return result;
+        }   
+
+        public static char Symb { get; set; } = '*';
+
+        public static void DrawRightTriange(int height)
+        {
+            if (height <= 0) throw new ArgumentException("This param must be greater than zero", "height");
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < i + 1; j++) Console.Write(Symb);
+                Console.WriteLine();
+            }
+        }
+
+        public static void DrawIsoscelesTriangle(int height, int offset = 0)
+        {
+            if (height <= 0) throw new ArgumentException("This param must be greater than zero", "height");
+            if (offset < 0) throw new ArgumentException("This param must be equal or greater than zero", "offset");
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < offset + height - i - 1; j++) Console.Write(' ');
+                for (int j = 0; j < 1 + (i * 2); j++) Console.Write(Symb);
+                Console.WriteLine();
+            }
+        }
+
+        public static void DrawXmasTree(int height)
+        {
+            if (height <= 0) throw new ArgumentException("This param must be greater than zero", "height");
+            for (int level = 1; level <= height; level++)
+            {
+                DrawIsoscelesTriangle(level, height - level);
+            }
+        }
+
+        public static int GetMaxElement(int[] array)
+        {
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++) if (max < array[i]) max = array[i];
+            return max;
+        }
+
+        public static int GetMinElement(int[] array)
+        {
+            int min = array[0];
+            for (int i = 1; i < array.Length; i++) if (min > array[i]) min = array[i];
+            return min;
+        }
+
+        public static int[] Generate1DArray(int length, int min_limit, int max_limit)
+        {
+            Random r = new Random();
+            int[] array = new int[length];
+            for (int i = 0; i < length; i++) array[i] = r.Next(min_limit, max_limit);
+            return array;
+        }
+
+        public static int[,] Generate2DArray(int dimension_1, int dimension_2, int min_limit, int max_limit)
+        {
+            Random r = new Random();
+            int[,] array = new int[dimension_1, dimension_2];
+            for (int i = 0; i < dimension_1; i++)
+            {
+                for (int j = 0; j < dimension_1; j++)
+                {
+                    array[i, j] = r.Next(min_limit, max_limit);
+                }
+            }
+            return array;
+        }
+
+        public static int[,,] Generate3DArray(int dimension_1, int dimension_2, int dimension_3, int min_limit, int max_limit)
+        {
+            Random r = new Random();
+            int[,,] array = new int[dimension_1, dimension_2, dimension_3];
+            for (int i = 0; i < dimension_1; i++)
+            {
+                for (int j = 0; j < dimension_1; j++)
+                {
+                    for (int k = 0; k < dimension_3; k++)
+                    {
+                        array[i, j, k] = r.Next(min_limit, max_limit);
+                    }
+                }
+            }
+            return array;
+        }
+
+        public static void ShowArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++) Console.Write("{0,2} ", array[i]);
+            Console.WriteLine();
+        }
+
+        public static void ShowArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write("{0,2} ", array[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void ShowArray(int[,,] array)
+        {
+            for (int d1 = 0; d1 < array.GetLength(0); d1++)
+            {
+                for (int d2 = 0; d2 < array.GetLength(1); d2++)
+                {
+                    for (int d3 = 0; d3 < array.GetLength(2); d3++)
+                    {
+                        Console.WriteLine("array[{0}, {1}, {2}] = {3,2}", d1, d2, d3, array[d1, d2, d3]);
+                    }
+                }
+            }
+        }
+
+        public static int[] ArraySort(int[] array, bool ascending = true)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 1; j < array.Length - i; j++)
+                {
+                    bool check = array[j] < array[j - 1];
+                    if (!ascending) check = !check;
+                    if (check)
+                    {
+                        int swap = array[j];
+                        array[j] = array[j - 1];
+                        array[j - 1] = swap;
+                    }
+                }
+            }
+            return array;
+        }
+
+
+        public static int[,,] ClearPositive(int[,,] array)
+        {
+            for (int d1 = 0; d1 < array.GetLength(0); d1++)
+            {
+                for (int d2 = 0; d2 < array.GetLength(1); d2++)
+                {
+                    for (int d3 = 0; d3 < array.GetLength(2); d3++)
+                    {
+                        if (array[d1, d2, d3] > 0) array[d1, d2, d3] = 0;
+                    }
+                }
+            }
+            return array;
+        }
+
+
+        public static int NonNegativeSum(int[] array)
+        {
+            int sum = 0;
+            foreach (int element in array) if (element >= 0) sum += element;
+            return sum;
+        }
+
+        public static int EvenSum(int[,] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                int step = (i % 2 == 0) ? 1 : 2;
+                for (int j = 0; j < array.GetLength(1); j += step)
+                {
+                    sum += array[i, j];
+                }
+            }
+            return sum;
+        }
+
+        public static int InputInt32()
+        {
+            int result;
+            while (!Int32.TryParse(Console.ReadLine(), out result)) Console.Write("You've entered not a number. Try again > ");
+            return result;
+        }
+
+        public static string Status((string, bool)[] parameters)
+        {
+            string status = "";
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                if (parameters[i].Item2) status += parameters[i].Item1 + ", ";  
+            }
+            return (String.IsNullOrEmpty(status)) ? "None" : status; 
+        }
+    }
+}
---
+++ Task 1/TheMagnificientTen/TheMagnificientTen/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace TheMagnificientTen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.1 - The Magnificient Ten");
+            while (true)
+            {
+                Console.Write("Enter task number (0 - if you want to exit) > ");
+                try 
+                {
+                    switch (Int32.Parse(Console.ReadLine()))
+                    {
+                        default: Console.WriteLine("You've entered non-existent task number."); break;
+                        case 0: return;
+                        case 1: Program1(); break;
+                        case 2: Program2(); break;
+                        case 3: Program3(); break;
+                        case 4: Program4(); break;
+                        case 5: Program5(); break;
+                        case 6: Program6(); break;
+                        case 7: Program7(); break;
+                        case 8: Program8(); break;
+                        case 9: Program9(); break;
+                        case 10: Program10(); break;
+                    }
+                    Console.WriteLine();
+                }
+                catch (Exception e) { Console.WriteLine("An error occured: {0}", e.Message); }
+            }
+        }
+
+        public static void Program1()
+        {
+            Console.WriteLine("Task #1 - Rectangle");
+
+            int a, b;
+            Console.Write("Enter side a > ");
+            a = Functions.InputInt32();
+            Console.Write("Enter side b > ");
+            b = Functions.InputInt32();
+
+            Console.WriteLine("Square of given rectagle = {0}", Functions.RectangleSqaure(a, b));
+        }
+
+        public static void Program2()
+        {
+            Console.WriteLine("Task #2 - Triangle");
+
+            Console.Write("Enter height of triangle > ");
+            Functions.DrawRightTriange(Functions.InputInt32());
+        }
+
+        public static void Program3()
+        {
+            Console.WriteLine("Task #3 - Another triangle");
+
+            Console.Write("Enter height of triangle > ");
+            Functions.DrawIsoscelesTriangle(Functions.InputInt32());
+        }
+
+        public static void Program4()
+        {
+            Console.WriteLine("Task #4 - X-mas tree");
+
+            Console.Write("Enter height of tree > ");
+            Functions.DrawXmasTree(Functions.InputInt32());
+        }
+
+        public static void Program5()
+        {
+            Console.WriteLine("Task #5 - Sum of numbers");
+
+            Console.WriteLine("Sum of numbers: {0}", Functions.SumOfNumbers(1000, new int[] { 3, 5 }));
+        }
+
+        public static void Program6()
+        {
+            Console.WriteLine("Task #6 - Font adjustment");
+            
+            (string, bool)[] settings = { ("Bold", false), ("Italic", false), ("Underline", false) };
+            int input;
+            while (true)
+            {
+                Console.WriteLine("Font parameters: " + Functions.Status(settings));
+                Console.WriteLine("Enter:\n1: bold\n2: italic\n3: underline");
+                input = Functions.InputInt32();
+                if (input == 0) break;
+                if (input < 4) settings[input - 1].Item2 = !settings[input].Item2;
+            }
+        }
+
+        public static void Program7()
+        {
+            Console.WriteLine("Task #7 - Array processing");
+
+            int[] array = Functions.Generate1DArray(15, 1, 100);
+            Functions.ShowArray(array);
+            Console.WriteLine("Maximum element: {0} | Minimum element: {1}", Functions.GetMaxElement(array), Functions.GetMinElement(array));
+            Console.WriteLine("Sorted ascending:");
+            Functions.ShowArray(Functions.ArraySort(array, true));
+            Console.WriteLine("Sorted descending:");
+            Functions.ShowArray(Functions.ArraySort(array, false));
+        }
+
+        public static void Program8()
+        {
+            Console.WriteLine("Task #8 - No positive");
+
+            int[,,] array = Functions.Generate3DArray(5, 5, 5, -50, 51);
+            Functions.ShowArray(array);
+            array = Functions.ClearPositive(array);
+            Console.WriteLine("Array with no positives:");
+            Functions.ShowArray(array);
+        }
+
+        public static void Program9()
+        {
+            Console.WriteLine("Task #9 - Non-negative sum");
+
+            int[] array = Functions.Generate1DArray(15, -50, 51);
+            Functions.ShowArray(array);
+            Console.WriteLine("Non-negative sum = {0}", Functions.NonNegativeSum(array));
+        }
+
+        public static void Program10()
+        {
+            Console.WriteLine("Task #10 - 2D array");
+
+            int[,] array = Functions.Generate2DArray(5, 5, 1, 10);
+            Functions.ShowArray(array);
+            Console.WriteLine("Sum of even elements = {0}", Functions.EvenSum(array));
+        }
+    }
+}

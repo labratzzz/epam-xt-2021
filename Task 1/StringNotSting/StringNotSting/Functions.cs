b33197c4a549f175e2d3c35896a5ae25033b3a9b
Michael Rodin <mikhail.rodin@satellite-soft.ru>
2021-03-03 19:57:07 +0400

    Added Task 1.2 exercises

---
+++ Task 1/StringNotSting/StringNotSting/Functions.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text;
+
+namespace StringNotSting
+{
+    class Functions
+    {
+        //Average number of words is not rounding
+        public static double AverageLettersInWords(string line)
+        {
+            int wordsCount = 0, lettersCount = 0;
+            bool countingLettersNow = true;
+            foreach (char ch in line)
+            {
+                if (Char.IsLetterOrDigit(ch))
+                {
+                    lettersCount++;
+                    if (!countingLettersNow)
+                    {
+                        wordsCount++;
+                        countingLettersNow = true;
+                    }
+                }
+                else if (countingLettersNow) countingLettersNow = false;
+            }
+            
+            if (lettersCount > 0) wordsCount++;
+            else return 0;
+
+            return (double)lettersCount / wordsCount;
+        }
+
+        public static string DoubleLetters(string lineA, string lineB)
+        {
+            StringBuilder lineABuilder = new StringBuilder(lineA);
+            StringBuilder lineBBuilder = new StringBuilder(lineB);
+            while (lineBBuilder.Length > 0)
+            {
+                lineABuilder.Replace(lineBBuilder[0].ToString(), lineBBuilder[0].ToString() + lineBBuilder[0]);
+                lineBBuilder.Replace(lineBBuilder[0].ToString(), "");
+            }
+            return lineABuilder.ToString();
+        }
+
+        public static int CountLowerWords(string line)
+        {
+            if (String.IsNullOrEmpty(line)) throw new ArgumentException("The value must be sting with at least one character");
+
+            int lowerCount = Char.IsLower(line[0]) ? 1 : 0;
+            bool runningOnLettersNow = true;
+            foreach (char ch in line)
+            {
+                if (Char.IsLetter(ch))
+                {
+                    if (!runningOnLettersNow)
+                    {
+                        if (Char.IsLower(ch)) lowerCount++;
+                        runningOnLettersNow = true;
+                    }
+                }
+                else if (runningOnLettersNow) runningOnLettersNow = false;
+            }
+
+            return lowerCount;
+        }
+
+        public static string FirstLetterOfSentenceToUpper(string line)
+        {
+            StringBuilder output = new StringBuilder();
+            bool capitalizeNext = true;
+
+            foreach (var ch in line)
+            {
+                if (capitalizeNext && char.IsLetter(ch))
+                {
+                    output.Append(char.ToUpper(ch));
+                    capitalizeNext = false;
+                    continue;
+                }
+
+                if (IsSentenceSeparator(ch)) capitalizeNext = true;
+
+                output.Append(ch);
+            }
+
+            return output.ToString();
+        }
+
+        public static bool IsSentenceSeparator(char ch)
+        {
+            foreach (char separator in SentenceSeparators) if (separator == ch) return true;
+            return false;
+        }
+
+        public static char[] SentenceSeparators = { '.', '?', '!' };
+    }
+}
---
+++ Task 1/StringNotSting/StringNotSting/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace StringNotSting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.2 - String not Sting");
+            while (true)
+            {
+                Console.Write("Enter task number (0 - if you want to exit) > ");
+                try
+                {
+                    switch (Int32.Parse(Console.ReadLine()))
+                    {
+                        default: Console.WriteLine("You've entered non-existent task number."); break;
+                        case 0: return;
+                        case 1: Program1(); break;
+                        case 2: Program2(); break;
+                        case 3: Program3(); break;
+                        case 4: Program4(); break;
+                    }
+                    Console.WriteLine();
+                }
+                catch (Exception e) { Console.WriteLine("An error occured: {0}", e.Message); }
+            }
+        }
+
+        public static void Program1()
+        {
+            Console.WriteLine("Task #1 - Averages");
+            
+            Console.Write("Enter string value > ");
+            Console.WriteLine("Average word length = {0:n1}", Functions.AverageLettersInWords(Console.ReadLine()));
+        }
+
+        public static void Program2()
+        {
+            Console.WriteLine("Task #2 - Doubler");
+
+            Console.Write("Enter string value #1 > ");
+            string lineA = Console.ReadLine();
+            Console.Write("Enter string value #2 > ");
+            string lineB = Console.ReadLine();
+            Console.WriteLine(Functions.DoubleLetters(lineA, lineB));
+        }
+
+        public static void Program3()
+        {
+            Console.WriteLine("Task #3 - Lowercase");
+
+            Console.Write("Enter string value > ");
+            Console.Write("Number of words starting with lowercase letter = {0}", Functions.CountLowerWords(Console.ReadLine()));
+        }
+
+        public static void Program4()
+        {
+            Console.WriteLine("Task #4 - Validator");
+
+            Console.Write("Enter string value > ");
+            Console.WriteLine(Functions.FirstLetterOfSentenceToUpper(Console.ReadLine()));
+        }
+    }
+}

e9f4077d9e8bcc00aff474c3edafc7250b701fcf
LabRatZ <labratz@yandex.ru>
2021-04-14 02:22:00 +0400

    Implemented Task 3.1.2

---
+++ Task 3/Task 3.1/TextAnalysis/TextAnalysis/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TextAnalysis
+{
+    public static class Program
+    {
+        public const string FileCommand = "file=";
+        public const string ExitCommand = "exit";
+        public static readonly string[] commands = { FileCommand, ExitCommand };
+        public static char[] charsToTrim = { ' ', '\'', '"', '/', '\\', ':', '*', '?', '>', '<', '|' };
+
+        public static void Main()
+        {
+            Console.WriteLine("Task 3.1.2 - Text Analysis");
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("To begin analysis type a few word in console or specify path to file.");
+                Console.WriteLine(@"To specify type 'file=C:\path\to\your\file.txt'");
+                Console.WriteLine("To exit type 'exit'");
+
+                string input = Console.ReadLine();
+                string text = input;
+
+                if (input.StartsWith(ExitCommand))
+                {
+                    break;
+                }
+                if (input.StartsWith(FileCommand))
+                {
+                    string path = input.Replace(FileCommand, string.Empty).Trim(charsToTrim);
+                    using (StreamReader reader = new StreamReader(path, Encoding.Unicode))
+                    {
+                        text = reader.ReadToEnd();
+                    }
+                }
+
+                var words = text.GetWords();
+                var analysis = words.GetAnalysis();
+
+                Console.WriteLine("Most Common Words:");
+                foreach (var pair in analysis)
+                {
+                    Console.WriteLine("'{0}' - {1}", pair.Key, pair.Value);
+                }
+            }
+        }
+    }
+}
---
+++ Task 3/Task 3.1/TextAnalysis/TextAnalysis/TextAnalyzer.cs
@@ -0,0 +1,90 @@
+﻿namespace TextAnalysis
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public enum TextAnalyzerCasing
+    {
+        ToUpper = 0,
+        ToLower = 1,
+        Original = 2
+    }
+
+    public enum TextAnalyzerMode
+    {
+        OnlyLetters = 0,
+        OnlyDigits = 1,
+        LettersOrDigits = 2
+    }
+
+    public static class TextAnalyzer
+    {
+        public static IEnumerable<string> GetWords(this string line, TextAnalyzerMode mode = TextAnalyzerMode.OnlyLetters, TextAnalyzerCasing casing = TextAnalyzerCasing.ToLower)
+        {
+            Predicate<char> IsDesired;
+            switch (mode)
+            {
+                default:
+                case TextAnalyzerMode.OnlyLetters:
+                    IsDesired = ch => char.IsLetter(ch);
+                    break;
+                case TextAnalyzerMode.OnlyDigits:
+                    IsDesired = ch => char.IsDigit(ch);
+                    break;
+                case TextAnalyzerMode.LettersOrDigits:
+                    IsDesired = ch => char.IsLetterOrDigit(ch);
+                    break;
+            }
+            
+            List<string> words = new List<string>();
+            int startIndex = 0;
+            bool runningOnDesiredNow = false;
+
+            for (int i = 0; i <= line.Length; i++) // TODO String.Empty returns one word
+            {
+                if (i < line.Length && IsDesired.Invoke(line[i]) )
+                {
+                    if (!runningOnDesiredNow)
+                    {
+                        startIndex = i;
+                        runningOnDesiredNow = true;
+                    }
+                }
+                else if (i >= line.Length || runningOnDesiredNow)
+                {
+                    string word = line.Substring(startIndex, i - startIndex);
+
+                    switch (casing)
+                    {
+                        default:
+                        case TextAnalyzerCasing.Original:
+                            break;
+                        case TextAnalyzerCasing.ToUpper:
+                            word = word.ToUpper();
+                            break;
+                        case TextAnalyzerCasing.ToLower:
+                            word = word.ToLower();
+                            break;
+                    }
+
+                    words.Add(word);
+
+                    runningOnDesiredNow = false;
+                }
+            }
+
+            return words;
+        }
+
+        public static IDictionary<string, int> GetAnalysis(this IEnumerable<string> words)
+        {
+            var analysis = words.GroupBy(w => w)
+                .Select(g => new { word = g.Key, quantity = g.Count() })
+                .OrderByDescending(g => g.quantity)
+                .ToDictionary(g => g.word, g => g.quantity);
+
+            return analysis;
+        }
+    }
+}
